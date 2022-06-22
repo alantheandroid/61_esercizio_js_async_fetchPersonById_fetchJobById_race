c53551a1b36c16b1e46fdec8e2d8687f7045c55e
alantheandroid <alessio.ilardi@gmail.com>
2022-06-22 11:50:32 +0200

    feat: add 'Promise.race' method

--- exercise-5.js
+++ exercise-5.js
@@ -1,37 +1,70 @@
 const persons = [
   {
     id: 1,
-    firstName: 'Mario',
-    lastName: 'Rossi',
-    age: 25
+    firstName: "Mario",
+    lastName: "Rossi",
+    age: 25,
   },
   {
     id: 2,
-    firstName: 'Maria',
-    lastName: 'Verdi',
-    age: 32
+    firstName: "Maria",
+    lastName: "Verdi",
+    age: 32,
   },
   {
     id: 3,
-    firstName: 'Giovanni',
-    lastName: 'Rossi',
-    age: 35
-  }
+    firstName: "Giovanni",
+    lastName: "Rossi",
+    age: 35,
+  },
 ];
 
 const jobs = [
   {
     id: 1,
-    jobTitle: 'CEO'
+    jobTitle: "CEO",
   },
   {
     id: 2,
-    jobTitle: 'Project Manager'
+    jobTitle: "Project Manager",
   },
   {
     id: 3,
-    jobTitle: 'Developer'
-  }
+    jobTitle: "Developer",
+  },
 ];
 
-// core here
+function fetchPersonByid(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let person = persons.find((item) => item.id === id);
+
+      if (person) {
+        return resolve(person);
+      }
+
+      return reject("l'id che hai inserito non è valido");
+    }, 1000);
+  });
+}
+
+function fetchJobByid(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let job = jobs.find((item) => item.id === id);
+
+      if (job) {
+        return resolve(job);
+      }
+
+      return reject("l'id che hai inserito non è valido");
+    }, 500);
+  });
+}
+
+const promise1 = fetchPersonByid(1);
+const promise2 = fetchJobByid(1);
+
+Promise.race([promise1, promise2]).then((values) => {
+  console.log(values);
+});
